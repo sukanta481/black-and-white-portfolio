625f38b8814edaf3cb16cca88c07f4edc9c19fe5
sukanta <sukantasaha481@gmail.com>
2022-06-07 22:54:55 +0530

    added some pic responsive

--- src/app.js
+++ src/app.js
@@ -2,3 +2,10 @@ const switchBtn = document.querySelector(".switch");
 switchBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+const menuBtn=document.querySelector(".menu");
+const navList=document.querySelector(".navList");
+menuBtn.addEventListener("click",()=>{
+    navList.classList.toggle("show");
+    // console.log("clicked");
+})
